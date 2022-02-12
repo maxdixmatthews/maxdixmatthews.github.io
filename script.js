adbb34664224032e8b443a4a0fa073af499a7abf
MDcitss <maxdixmatthews@gmail.com>
2022-02-12 22:28:41 +0800

    fix delete css

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function divMaker(text){
     h2_answer.innerHTML = text[1];
 
     delCard.className = "fa fa-close"
-    delCard.setAttribute('style',"position: absolute; padding-top:10px; padding-left: 90%; color:red")
+    delCard.setAttribute('style',"position: absolute; padding-bttom:90%; padding-left: 90%; color:red")
     // delCard.setAttribute('onclick','deleteCard')
 
     speaker.className = "fas fa-volume-up"; 
